8488b359f678ea72b2b7d280b292a8fcd1732471
Remy Hidra <remy.hidra@gmail.com>
2017-12-14 08:34:17 +0100

    Delete agp and sde folder

--- agp/tdgdb/td5.h
+++
@@ -1,5 +0,0 @@
-ARBRE nouvelArbre(int val, ARBRE fg, ARBRE fd);
-int hauteurArbre(ARBRE arbre);
-void afficherArbre(ARBRE arbre, int decalage);
-int arbreEquilibre (ARBRE arbre);
-
--- agp/tp_exam_2016/array.h
+++
@@ -1,8 +0,0 @@
-#ifndef ARRAY_H
-#define ARRAY_H
-
-void flattenArray(int tab[], int size);
-int getMin(int tab[], int size);
-void printArray(int tab[], int size);
-
-#endif
