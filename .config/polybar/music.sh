6ba46ebe6b6dfe5db44ed8255702d37fbe803681
Remy Hidra <remy.hidra@gmail.com>
2017-12-26 21:56:09 +0100

    Affichage des musiques sans tags

--- .config/polybar/music.sh
+++ .config/polybar/music.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-name=$(mocp -Q "%song - %artist  |  %ct / %tl")
+limit=70
+
+song=$(mocp -Q "%song")
+artist=$(mocp -Q "%artist")
+if [ -z "$song" ]; then
+  name=$(mocp -Q "%file")
+  name=${name#'/media/files/Musique/'}
+  if [ ${#name} -gt $limit ]; then
+    name=${name:$limit-${#name}:$limit}
+  fi
+elif [ -z "$artist" ]; then
+  name=$song
+else
+  name="$song - $artist"
+fi
+
+name="$name  |  $(mocp -Q "%ct / %tl")"
 state=$(mocp -Q "%state")
 
 if [ $state = "PLAY" ]; then
