9dffac41d319831922ab0697f4a5167a604818dc
Remy Hidra <remy.hidra@gmail.com>
2017-12-27 00:45:05 +0100

    Change sound threshold

--- .config/polybar/sound.sh
+++ .config/polybar/sound.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+limit=15
+
 level=$(/usr/share/i3blocks/volume 5 pulse)
 level=${level:0:${#level}-1}
 
 if [[ "$level" =~ "MUT" ]]; then
   echo ""
-elif [ $level -lt 10 ]; then
+elif [ $level -lt $limit ]; then
   echo " $level"
 else 
   echo " $level"
