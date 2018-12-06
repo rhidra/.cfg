d63d5da90d291061493661be9ac3432c6a09037e
Rémy Hidra <remy.hidra@gmail.com>
2018-12-07 00:48:30 +0900

    Install Mint

--- .config/polybar/sound.sh
+++ .config/polybar/sound.sh
@@ -2,13 +2,14 @@
 
 limit=15
 
-level=$(/usr/share/i3blocks/volume 5 pulse)
+# level=$(/usr/share/i3blocks/volume 5 pulse)
+level=$(amixer -c 0 -M -D pulse get Master | ruby -ne '$_ =~ /\[(\d+)%\] \[(on|off)\]/ and (puts $2 == "off" ? "MUTE" : "#$1%"; exit)')
 level=${level:0:${#level}-1}
 
 if [[ "$level" =~ "MUT" ]]; then
   echo ""
 elif [ $level -lt $limit ]; then
   echo " $level"
-else 
+else
   echo " $level"
 fi
--- reinstall.sh
+++ reinstall.sh
@@ -5,7 +5,7 @@ cd $HOME
 # apt install bcmwl-kernel-source
 
 # Luminosité
-apt -y --force-yes install automake autoconf
+apt install -y automake autoconf
 git clone https://github.com/haikarainen/light.git
 cd light
 ./autogen.sh
@@ -15,16 +15,20 @@ cd ..
 rm -rf light
 
 # Applications de base
-apt -y --force-yes install thunderbird vim vlc git curl feh ranger scrot build-essential
+apt install -y thunderbird vim vlc git curl feh ranger scrot build-essential evince
 
 # Python
-apt -y --force-yes install ipython3 python3-pip python3-tk
+apt install -y ipython3 python3-pip python3-tk
 pip3 install numpy matplotlib
+
+# Police caractères asiatiques
+apt install -y fonts-nanum
 
 # Atom
 curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
-apt -y --force-yes update
+apt update -y
+apt install -y atom
 
 # Playerctl
 wget -v "https://github.com/acrisci/playerctl/releases/download/v0.5.0/playerctl-0.5.0_amd64.deb"
@@ -36,17 +40,17 @@ wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 dpkg -i gitkraken-amd64.deb
 
 # i3
-apt -y --force-yes install i3 compton rofi
+apt install -y i3 compton rofi
 
 # Polybar
-apt -y --force-yes install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev
+apt install -y cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev
 git clone https://github.com/jaagr/polybar.git
-cd polybar && ./build.sh --i3 --network --curl --ipc
+cd polybar && ./build.sh --auto --i3 --network --curl --ipc
 cd ..
 rm -rf polybar
 
 # urxvt
-apt -y --force-yes install rxvt-unicode-256color autocutsel
+apt install -y rxvt-unicode-256color autocutsel
 
 # Fichiers de configuration
 echo ".cfg" >> .gitignore
@@ -58,8 +62,3 @@ config checkout
 
 fc-cache -fv
 xrdb $HOME/.Xresources
-
-
-lxappearance
-libnotify-bin
-libav-tools
